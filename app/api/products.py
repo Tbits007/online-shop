134dbb1776feff37166437a23859541cd2ac1fa9
brusolis228@gmail.com <brusolis228@gmail.com>
2024-10-26 00:12:00 +0300

    added endpoints structure

--- app/api/products.py
+++ app/api/products.py
@@ -5,3 +5,43 @@ router = APIRouter(
     prefix="/products",
     tags=["Products"],
 )
+
+
+@router.post("/create_product/")
+def create_product():
+    """
+    Создать новый продукт.
+    """
+    pass
+    
+
+@router.get("/")
+def get_products():
+    """
+    Получить список всех продуктов.
+    """
+    pass
+
+
+@router.get("/{id}/")
+def get_product():
+    """
+    Получить информацию о продукте по ID.
+    """
+    pass 
+
+
+@router.put("/{id}/")
+def update_product():
+    """
+    Обновить информацию о продукте по ID.
+    """
+    pass
+
+
+@router.delete("/{id}/")
+def delete_product():
+    """
+    Удалить продукт по ID.
+    """
+    pass
