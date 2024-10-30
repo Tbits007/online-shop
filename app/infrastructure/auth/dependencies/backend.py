3ffa37fe13ab896cf38cb622fd3d9a4037800abc
brusolis228@gmail.com <brusolis228@gmail.com>
2024-10-30 22:57:40 +0300

    create authentication backend

---
+++ app/infrastructure/auth/dependencies/backend.py
@@ -0,0 +1,9 @@
+from fastapi_users.authentication import AuthenticationBackend
+from app.infrastructure.auth.dependencies.strategy import get_database_strategy
+from app.infrastructure.auth.transport import bearer_transport
+
+authentication_backend = AuthenticationBackend(
+    name="access-token-db",
+    transport=bearer_transport,
+    get_strategy=get_database_strategy,
+)
