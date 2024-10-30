c3507b031a6e1f5c5e953c6ac14df2a70c55e88b
brusolis228@gmail.com <brusolis228@gmail.com>
2024-10-31 00:09:51 +0300

    fixed bugs with settings

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.infrastructure.auth import auth
 app = FastAPI()
 
 # Подключение маршрутов
+app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(categories.router)
 app.include_router(products.router)
 app.include_router(orders.router)
-app.include_router(auth.router)
 
 # uvicorn app.main:app --reload
