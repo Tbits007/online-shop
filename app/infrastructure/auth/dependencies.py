c2a56a6d59e6a3c1b5fa86e016656b05fd0721fb
brusolis228@gmail.com <brusolis228@gmail.com>
2024-10-30 17:07:06 +0300

    +adapter for working with access token

--- app/infrastructure/auth/dependencies.py
+++ app/infrastructure/auth/dependencies.py
@@ -2,11 +2,22 @@ from fastapi import Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.domain.users import Users
+from app.infrastructure.auth.models import AccessToken
 from app.infrastructure.database import get_session
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyAccessTokenDatabase,
+)
 
 
 async def get_user_db(session: AsyncSession = Depends(get_session)):
     """
-    Адаптер для работы с пользователями.
+    Получение адаптера для работы с пользователями.
     """
     yield SQLAlchemyUserDatabase(session, Users)
+
+
+async def get_access_token_db(session: AsyncSession = Depends(get_session)):
+    """
+    Получение адаптера для работы с access token.
+    """  
+    yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
---
+++ app/infrastructure/auth/models.py
@@ -0,0 +1,9 @@
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyBaseAccessTokenTableUUID,
+)
+
+from app.infrastructure.database import Base
+
+
+class AccessToken(SQLAlchemyBaseAccessTokenTableUUID, Base):  
+    pass
