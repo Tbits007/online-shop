81513e03a4d1a55ea0163157ef12f183214248c4
brusolis228@gmail.com <brusolis228@gmail.com>
2024-10-30 17:17:21 +0300

    +some edits

--- app/infrastructure/auth/models.py
+++ app/infrastructure/auth/models.py
@@ -1,9 +1,15 @@
 from fastapi_users_db_sqlalchemy.access_token import (
     SQLAlchemyBaseAccessTokenTableUUID,
 )
-
 from app.infrastructure.database import Base
+from fastapi_users_db_sqlalchemy.generics import GUID
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
 
 
 class AccessToken(SQLAlchemyBaseAccessTokenTableUUID, Base):  
-    pass
+    user_id: Mapped[GUID] = mapped_column(
+        GUID,
+        ForeignKey("users.id", ondelete="cascade"),
+        nullable=False
+    )
