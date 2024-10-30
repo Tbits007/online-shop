58ade2679cef68d8d89be5c641d8f97850771ac1
brusolis228@gmail.com <brusolis228@gmail.com>
2024-10-30 23:32:34 +0300

    remake Users schemas

--- app/schemas/users_schemas.py
+++ app/schemas/users_schemas.py
@@ -1,30 +1,14 @@
-from typing import Union
-from uuid import UUID
-from pydantic import BaseModel, EmailStr
+import uuid
+from fastapi_users import schemas
 
 
-class UserCreateSchema(BaseModel):
-    email: EmailStr
-    password: str
-    is_active: bool = True
-    is_superuser: bool = False
-    is_verified: bool = False
+class UserCreateSchema(schemas.BaseUserCreate):
+    pass
 
 
-class UserUpdateSchema(BaseModel):
-    email: EmailStr
-    password: str
-    is_active: Union[bool, None] = None
-    is_superuser: Union[bool, None] = None
-    is_verified: Union[bool, None] = None
+class UserUpdateSchema(schemas.BaseUserUpdate):
+    pass
 
 
-class UserResponseSchema(BaseModel):
-    id: UUID
-    email: EmailStr
-    is_active: bool
-    is_superuser: bool
-    is_verified: bool
-
-    class Config:
-        from_attributes = True
+class UserResponseSchema(schemas.BaseUser[uuid.UUID]):
+    pass
