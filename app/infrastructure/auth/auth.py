7ea62a3222c9bed0f228065161416bb9152e627d
brusolis228@gmail.com <brusolis228@gmail.com>
2024-10-31 18:38:24 +0300

    add register route

--- app/infrastructure/auth/auth.py
+++ app/infrastructure/auth/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.infrastructure.auth.dependencies.FastAPIUsersObject import fastapi_users
 from app.infrastructure.auth.dependencies.backend import authentication_backend
+from app.schemas.users_schemas import UserCreateSchema, UserResponseSchema
 
 
 router = APIRouter(
@@ -8,6 +9,12 @@ router = APIRouter(
     tags=["Auth"],
 )
 
+# /login & /logout
 router.include_router(
     fastapi_users.get_auth_router(authentication_backend),
+)
+
+# /register
+router.include_router(
+    fastapi_users.get_register_router(UserResponseSchema, UserCreateSchema),
 )
