182e584348c4a2e78a80b920c78887d3c5d383f4
brusolis228@gmail.com <brusolis228@gmail.com>
2024-11-01 23:56:57 +0300

    add tests folders

--- app/infrastructure/config.py
+++ app/infrastructure/config.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -9,12 +10,20 @@ class AccessToken(BaseModel):
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str 
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
-    
+
+    TEST_DB_HOST: str 
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+       
     access_token: AccessToken
     
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
@@ -22,6 +31,11 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+    @property
+    def TEST_DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
 
 settings = Settings()
